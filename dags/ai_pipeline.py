37f88d14f8e1d1aafe5e0bab1c575a9e2b93f6d1
Aditya Aggarwal <126685999+adi24z@users.noreply.github.com>
2026-05-27 03:27:57 +0530

    Change default email for privacy :)

--- dags/ai_pipeline.py
+++ dags/ai_pipeline.py
@@ -256,7 +256,7 @@ def send_email_task(**context):
     print(url)
 
     send_email(
-        recipient=context["dag_run"].conf.get("email", "ceogoogl123@gmail.com"),
+        recipient=context["dag_run"].conf.get("email", "DefaultEmail@gmail.com"),
         download_url=url
     )
 
